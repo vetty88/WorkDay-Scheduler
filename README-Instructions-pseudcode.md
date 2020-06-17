9e529f956255f52e7edb0a0abb5f6dbbaf4a5c5e
Vetty <vetty88@gmail.com>
2020-06-17 10:58:25 +1000

    overhaul html and javascript start again

--- README-Instructions-pseudcode.md
+++ README-Instructions-pseudcode.md
@@ -14,36 +14,19 @@ SO THAT I can manage my time effectively
 
 ## Acceptance Criteria
 
-Resource - https://medium.com/@aleks.roslyakov/using-css-grid-jquery-making-a-daily-scheduler-pt-ii-7af7d239a55d
-
 ```
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
-<!-- create planner template day - hours -->
-<!-- launch JS Query page on load - $(document).ready(function(){}) -->
-<!-- Javascript date function parse into year month day -->
-<!-- div class container h2 class date class calendar --> ?heaeder contains text from output variable for date.
 THEN the current day is displayed at the top of the calendar
-<!-- launch moments JS -->
-<!-- display current date at top of calendar -->
 WHEN I scroll down
-<!-- on scroll - display timeblocks 
-<!-- option values for hours -->
-timeblocks display standard business hours -->
 THEN I am presented with timeblocks for standard business hours
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-<!-- timeblock is coded per moment js past present or future from current -->
-<!-- use CSS grid -->
 WHEN I click into a timeblock
-<on click - enter event>
 THEN I can enter an event
 WHEN I click the save button for that timeblock
-<on click save button for time block .. add cancel option to escape>
 THEN the text for that event is saved in local storage
-<!-- save text event in local storage -->
 WHEN I refresh the page
-<!-- refresh page - saved event persists -->
 THEN the saved events persist
 ```
 
@@ -61,3 +44,26 @@ You are required to submit the following for review:
 
 - - -
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
+## PseudoCode
+```
+<!-- Insert HTML instruction to plan your daily schedule -->
+<!-- when HTML opens - display current date and time --> COMPLETE
+<!-- Allow scroll function -->
+<!-- Create timeblocks for Standard business hours -->
+<!-- Colour code time -->
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
--- script.js
+++ script.js
@@ -1,30 +1,141 @@
-$(document).ready(function() {   
+$(document).ready(function() {
+    
 var today = (moment().format('Do MMMM YYYY'));
-var time = (moment().format('HH:MM'));
+var time = (moment().format('HH')); 
+var rows = ['0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800'];
+         
+
 
 console.log(today);
 console.log(time);
+console.log(rows.length);
 
 $("#today").append(today);
-
 $("#timeNow").append(time);
-
-var hours = [0800, 0900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800];
-console.log(hours);
-
-var hoursString = (JSON.stringify(hours));
-console.log(hoursString);
-
-var hoursView = document.getElementById("#hours-view");
-console.log(hoursView);
-
-function RenderHourRows(){
-for (var i = 0; i <hours.length; i++) {
-var newHourRow = $("<row>") + text(hours, hoursString)
-$("#hours-view").append(newHourRow);
-};};
-
-console.log(RenderHourRows);
-console.log(newHourRow);
-
 });
+
+for ()
+
+// for(i = 0; i < rows.length; i++)      
+
+//     //manipulate rows 
+  
+//         if (rows < time){ 
+  
+//           rows[i].className = "even"; 
+  
+//         }else{ 
+  
+//           rows[i].className = "odd"; 
+  
+//         }       
+
+
+
+// var hours = ['0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800'];
+// var tasks = ['task 1', 'task 2', 'task 3'];
+// console.log(hours);
+// console.log(tasks);
+
+// document.getElementById("#hours-view");
+// document.getElementById("#tasks-view");
+
+// function renderHourRows() {
+//     for (var i = 0; i < hours.length; i++) {
+//         var hr = hours[i];
+//         console.log(hr);
+//         var newHourRow = "<li>" + hr + "</li>";
+//     }
+//         $("#hours-view").append(newHourRow);
+// };
+
+// renderHourRows();
+
+// function renderTaskRows(){
+//     for (var i = 0; i < tasks.length; i++) {
+//         var task = tasks[i];
+//         console.log(task);
+//         var newTaskRow = "<li>" + task + "</li>";
+//     }
+//     $("#tasks-view").append(newTaskRow);
+// };
+
+// renderTaskRows();
+
+// });
+
+// var eventInput = document.getElementById('#enter-task');
+
+// localStorage.setItem("event",eventInput.value);
+
+// submitBtn.addEventListener('click', function() {
+//     localStorage.setItem('eventInput', eventInput.value)});
+//     alert("storedEvent");
+
+
+// // var submitBtn = document.querySelector("#submitBtn");
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault()});
+
+// submitBtn.addEventListener('click', function() {
+//     localStorage.setItem('eventInput', eventInput.value)});
+//     alert("storedEvent");
+//     function checkForEnter(eventInput) {
+//         if (eventInput.key === "Enter") 
+//         {saveEventText("")};
+//         submitBtn.onclick = saveEventText;}
+//         checkForEnter();
+    
+//     function saveEventText(){
+//         if (eventText = "") 
+//         return eventText("")};
+
+        // var storedEvent = localStorage.getItem("event");
+
+//     var events = JSON.parse(window.localStorage.getItem("events")) || [];
+//     var newTask = "event"
+//     events.push(newEvent);
+//     window.localStorage.setItem("newEvents", JSON.stringify(newEvents));
+//     window.location.href = "index.html";
+
+//     function printEvents() {
+//     var events = JSON.parse(window.localStorage.getItem("events")) || [];
+//     events.sort(function(a, b) {
+//     return b.event - a.event;
+//     });
+    
+//         events.forEach(function(event) {
+//           // create li tag for each event
+//         var liTag = document.createElement("li");
+//         liTag.textContent = event.text + " - " + event.event;
+//       // display on page
+//         var olEl = document.getElementById("events");
+//         olEl.appendChild(liTag);
+//         });
+// }
+
+//     function clearEvents() {
+//         window.localStorage.removeItem("events");
+//         window.location.reload();
+//     }
+    
+
+//       // run function when page loads
+// printEvents();
+
+// var clearBtn = document.querySelector("#clearBtn");
+// clearBtn.onclick = clearEvents;
+
+// init();
+
+    // function init() {
+    //     storeTodaysDate();
+    //     changeDay();
+    //     updateTime();
+    //     displaySchedule();
+    //     scheduleFocus();
+    //     saveEvent();
+    //     clearSchedule();
+    // }
+
+
