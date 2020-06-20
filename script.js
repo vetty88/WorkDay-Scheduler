ea2b906d6651bf548a4a93dde2491542222ae3b3
Vetty <vetty88@gmail.com>
2020-06-20 15:32:28 +1000

    tidied jquery

--- script.js
+++ script.js
@@ -1,94 +1,116 @@
 $(document).ready(function() {
 
-        var today = (moment().format('Do MMMM YYYY'));
-        var time = (moment().format('HH:MM')); 
-        var currentHour = moment().hours();
-        var i = 0;
-       
-        $("#today").append(today);
-        $("#timeNow").append(time);
+$(window).on("load", function() {
+    colorChange();
+    saveFunction();;
+    });
 
-        console.log(today);
-        console.log(time);
-        console.log(currentHour);
-        console.log(i);
-           
-
-        function greeting() {
-            var currentHour = moment().hours();
-                var greeting;
-                if (currentHour < 12) {
-                        greeting = "Good morning, have a beautiful day!";
-                }
-                else if (currentHour < 20) {
-                        greeting = "Good afternoon, let's seize the day!";
-                } 
-                else {
-                        greeting = "Good evening, rest up for another day of success!";
-                }
-                document.getElementById("demo").innerHTML = greeting;
-                }
-                greeting();
+    var today = (moment().format('Do MMMM YYYY'));
+    var time = (moment().format('HH:MM')); 
+    var currentHour = moment().hours();
+    var i = 0;
     
-                
+    $("#today").append(today);
+    $("#timeNow").append(time);
+    
+    console.log(today);
+    console.log(time);
+    console.log(currentHour);
+    console.log(i);
+    
+    function greeting() {
+        var currentHour = moment().hours();
+        var greeting;
+            if (currentHour < 12) {
+                greeting = "Good morning, have a beautiful day!";
+            }
+            else if (currentHour < 20) {
+                    greeting = "Good afternoon, let's seize the day!";
+            } 
+            else {
+                    greeting = "Good evening, rest up for another day of success!";
+            }
+            document.getElementById("demo").innerHTML = greeting;
+            }
+            greeting();
+                    
     function colorChange() {
-        // creating current hour
-
-        // console.log(currentHour);
-
-        // creating color for past, present and future
         $(".time-block").each(function () {
-            var hourNum = parseInt($(this).attr("id"));
-            var currentHour = moment().hours();
+        var hourNum = parseInt($(this).attr("id"));
+        var currentHour = moment().hours();
             console.log(hourNum, currentHour);
 
-            // Comparing the time
             if (hourNum < currentHour) {
                 $(this).parent('tr').addClass("past");
-            } else if (hourNum === currentHour) {
+            } 
+            else if (hourNum === currentHour) {
                 $(this).parent('tr').removeClass("past");
                 $(this).parent('tr').addClass("present");
-            } else {
+            } 
+            else {
                 $(this).parent('tr').removeClass("past");
                 $(this).parent('tr').removeClass("present");
                 $(this).parent('tr').addClass("future");
             }
         });
-
     }
 
-    colorChange();
-    saveFunction();
-
-    
-});
-
-$(function clearEvents() {
-    $(".btn-success").click(function () {
-    console.log(localStorage);
-    localStorage.clear();
-    
-    })
-    location.reload(true);
+    deleteFunction();
     clearEvents();
-})
 
-function saveFunction() {
+    $(function clearEvents() {
+    $(".btn-success").click(function () {
+            console.log(localStorage);
+        localStorage.clear();
+    })
+    })
+
+    $(function deleteFunction() {
+    $(".btn-danger").click(function () {
+        var directParent = $(this).parent();
+            console.log("directParent", directParent);
+        var th = directParent.siblings("th");
+            console.log("th", th);
+        var time = th.attr("id");
+            console.log("time", time);
+            console.log("time");
+            console.log(time);
+        var rowTd = directParent.parent();
+            console.log("rowTd", rowTd);
+        var text = rowTd.find("textarea").val();
+            console.log(text);
+        window.localStorage.removeItem(time, text);
+        location.reload(true);
+    })
+
+    ("#row-8")(window.localStorage.removeItem("8"));
+    ("#row-9")(window.localStorage.removeItem("9"));
+    ("#row-10")(window.localStorage.removeItem("10"));
+    ("#row-11")(window.localStorage.removeItem("11"));
+    ("#row-12")(window.localStorage.removeItem("12"));
+    ("#row-13")(window.localStorage.removeItem("13"));
+    ("#row-14")(window.localStorage.removeItem("14"));
+    ("#row-15")(window.localStorage.removeItem("15"));
+    ("#row-16")(window.localStorage.removeItem("16"));
+    ("#row-17")(window.localStorage.removeItem("17"));
+    });
+
+    function saveFunction() {
     $(".btn-primary").click(function () {
         event.preventDefault();
 
         var directParent = $(this).parent();
-        console.log("directParent", directParent);
+            console.log("directParent", directParent);
         var th = directParent.siblings("th");
-        console.log("th", th);
+            console.log("th", th);
         var time = th.attr("id");
-        console.log("time", time);
-        console.log("time");
-        console.log(time);
+            console.log("time", time);
+            console.log("time");
+            console.log(time);
         var rowTd = directParent.parent();
-        console.log("rowTd", rowTd);
+            console.log("rowTd", rowTd);
         var text = rowTd.find("textarea").val();
-        console.log(text);
+            console.log(text);
         localStorage.setItem(time, text);
     })
 
@@ -106,36 +128,4 @@ function saveFunction() {
     console.log(localStorage);
     console.log(window.localStorage);
 
-    
-$(function deleteFunction() {
-    $(".btn-danger").click(function () {
-        
-        var directParent = $(this).parent();
-        console.log("directParent", directParent);
-        var th = directParent.siblings("th");
-        console.log("th", th);
-        var time = th.attr("id");
-        console.log("time", time);
-        console.log("time");
-        console.log(time);
-        var rowTd = directParent.parent();
-        console.log("rowTd", rowTd);
-        var text = rowTd.find("textarea").val();
-        console.log(text);
-        window.localStorage.removeItem(time, text);
-        location.reload(true);
-    })
-
-    ("#row-8")(window.localStorage.removeItem("8"));
-    ("#row-9")(window.localStorage.removeItem("9"));
-    ("#row-10")(window.localStorage.removeItem("10"));
-    ("#row-11")(window.localStorage.removeItem("11"));
-    ("#row-12")(window.localStorage.removeItem("12"));
-    ("#row-13")(window.localStorage.removeItem("13"));
-    ("#row-14")(window.localStorage.removeItem("14"));
-    ("#row-15")(window.localStorage.removeItem("15"));
-    ("#row-16")(window.localStorage.removeItem("16"));
-    ("#row-17")(window.localStorage.removeItem("17"));
-    });
-    deleteFunction(); 
-};
+    }})
