b52bbce835c56ab670d71980d6532be71d23e4cc
Vetty <vetty88@gmail.com>
2020-06-20 17:45:35 +1000

    updated js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
             }
             colorChange();
         }    
-    );
+    )})
     // save typed events for each hour when save button clicked
 
     $(function saveFunction() {
@@ -56,22 +56,22 @@ $(document).ready(function() {
         var rowTd = directParent.parent();
         var text = rowTd.find("textarea").val();
         localStorage.setItem(time, text);
-    }
+    })})
     
-    ("#row-8").val(localStorage.getItem("8"))
-    ("#row-9").val(localStorage.getItem("9"))
-    ("#row-10").val(localStorage.getItem("10"))
-    ("#row-11").val(localStorage.getItem("11"))
-    ("#row-12").val(localStorage.getItem("12"))
-    ("#row-13").val(localStorage.getItem("13"))
-    ("#row-14").val(localStorage.getItem("14"))
-    ("#row-15").val(localStorage.getItem("15"))
-    ("#row-16").val(localStorage.getItem("16"))
-    ("#row-17").val(localStorage.getItem("17"))
+    $("#row-8").val(localStorage.getItem("8"));
+    $("#row-9").val(localStorage.getItem("9"));
+    $("#row-10").val(localStorage.getItem("10"));
+    $("#row-11").val(localStorage.getItem("11"));
+    $("#row-12").val(localStorage.getItem("12"));
+    $("#row-13").val(localStorage.getItem("13"));
+    $("#row-14").val(localStorage.getItem("14"));
+    $("#row-15").val(localStorage.getItem("15"));
+    $("#row-16").val(localStorage.getItem("16"));
+    $("#row-17").val(localStorage.getItem("17"));
     
     
 
-    (function deleteFunction() {
+    $(function deleteFunction() {
         $(".btn-danger").click(function () {
             var directParent = $(this).parent();
             var th = directParent.siblings("th");
@@ -80,24 +80,23 @@ $(document).ready(function() {
             var text = rowTd.find("textarea").val();
                 window.localStorage.removeItem(time, text);
                 location.reload(true);
-    }
+        })})
 
-    ("#row-8")(window.localStorage.removeItem("8"))
-    ("#row-9")(window.localStorage.removeItem("9"))
-    ("#row-10")(window.localStorage.removeItem("10"))
-    ("#row-11")(window.localStorage.removeItem("11"))
-    ("#row-12")(window.localStorage.removeItem("12"))
-    ("#row-13")(window.localStorage.removeItem("13"))
-    ("#row-14")(window.localStorage.removeItem("14"))
-    ("#row-15")(window.localStorage.removeItem("15"))
-    ("#row-16")(window.localStorage.removeItem("16"))
-    ("#row-17")(window.localStorage.removeItem("17"))
-    )
+    $("#row-8")(window.localStorage.removeItem("8"));
+    $("#row-9")(window.localStorage.removeItem("9"));
+    $("#row-10")(window.localStorage.removeItem("10"));
+    $("#row-11")(window.localStorage.removeItem("11"));
+    $("#row-12")(window.localStorage.removeItem("12"));
+    $("#row-13")(window.localStorage.removeItem("13"));
+    $("#row-14")(window.localStorage.removeItem("14"));
+    $("#row-15")(window.localStorage.removeItem("15"));
+    $("#row-16")(window.localStorage.removeItem("16"));
+    $("#row-17")(window.localStorage.removeItem("17"));
+    
 
     $(function clearEvents() {
         $(".btn-success").click(function () {
             localStorage.clear();
         })
         clearEvents()
-        })
-    }))})})})
+    })})
